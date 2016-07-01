ae28dbd1b453584760450caf71230ee8c6fbf09f
Florian <florian-fischer@posteo.de>
2016-07-01 21:12:27 +0200

    Added guards to ensure multiple usage

--- devbox/bootstrap.sh
+++ devbox/bootstrap.sh
@@ -6,25 +6,31 @@ if [ "$EUID" -ne "0" ] ; then
         exit 1
 fi
 
-if which ansible > /dev/null ; then
-        echo "ansible is already installed"
-        exit 0
+if [ ! -s /etc/pacman.d/mirrorlist.backup ]; then
+    # Sort pacman mirrors by speed and use the 6 fastest ones
+    cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+    sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
+    # remove last 3 line from pacman.conf as these contain a hardcoded mirror
+    if [ ! -s /etc/pacman.conf.backup ]; then
+        cp /etc/pacman.conf /etc/pacman.conf.backup
+        head -n -3 /etc/pacman.conf.backup > /etc/pacman.conf
+    fi
+    echo "Searching fastest mirrors. This might take a while"
+    rankmirrors -n 10 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
+    # Alternatively we can pull a list for Germany from here:
+    # https://www.archlinux.org/mirrorlist/?country=DE&protocol=https&ip_version=4&ip_version=6&use_mirror_status=on
+    # wget -O /etc/pacman.d/mirrorlist https://www.archlinux.org/mirrorlist/?country=DE&protocol=https&ip_version=4&ip_version=6&use_mirror_status=on
 fi
-
-# Sort pacman mirrors by speed and use the 6 fastest ones
-cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
-echo "Searching fastest mirrors. This might take a while"
-rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
-# Alternatively we can pull a list for Germany from here:
-# https://www.archlinux.org/mirrorlist/?country=DE&protocol=https&ip_version=4&ip_version=6&use_mirror_status=on
-# wget -O /etc/pacman.d/mirrorlist https://www.archlinux.org/mirrorlist/?country=DE&protocol=https&ip_version=4&ip_version=6&use_mirror_status=on
-
 echo "Updating package list"
 pacman -Sy
-echo "Installing ansible"
-pacman -S --noconfirm ansible
-echo "ansible installed!"
+if which ansible > /dev/null ; then
+        echo "ansible is already installed"
+else
+        echo "Installing ansible"
+        pacman -S --noconfirm ansible
+        echo "ansible installed!"
+fi
+
 echo "Running ansible"
 cd /vagrant
 ansible-playbook -i hosts site.yml
